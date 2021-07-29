dcad878f60c604c6e6128958bbfa29276c9e3856
Tunji <54449991+Adetunjicode@users.noreply.github.com>
2021-07-29 15:47:05 +0100

    made some changes in animation.js

--- animation.js
+++ animation.js
@@ -1,13 +1,6 @@
 window.onload = beginningAnimation();
 
-let buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
-const main = document.querySelector("main");
-const endAlrt = document.querySelector("#end-alert");
-const endDesc = document.querySelector("#end-desc");
-const returnMainBtn = document.querySelector("#retry-btn");
-const desc = document.querySelector("#desc3");
-const container = document.querySelector("#results-container");
 const playBtn = document.querySelector('.playBtn');
 
 body.addEventListener("click", skipAnime());
